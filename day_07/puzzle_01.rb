1679c461c887c3e52f9ee2fc44d65529078840f9
Carl Gustafsson <neonpalm23@gmail.com>
2023-12-09 10:51:21 +0100

    Reverse score and card rank already instead of reversing array later.

--- day_07/puzzle_01.rb
+++ day_07/puzzle_01.rb
@@ -1,15 +1,15 @@
-CARD_RANK = "AKQJT987654321".freeze
+CARD_RANK = "23456789TJQKA".freeze
 find_type = ->(hand) do
     hand_values = hand.split("").tally.values
     case hand_values
-    when -> (values) { values.length == 1 } then { name: :five_of_a_kind, score: 1 }
-    when -> (values) { values.include?(4) } then { name: :four_of_a_kind, score: 2 }
-    when -> (values) { ([2, 3] & values).size == 2 } then { name: :full_house, score: 3 }
+    when -> (values) { values.length == 1 } then { name: :five_of_a_kind, score: 7 }
+    when -> (values) { values.include?(4) } then { name: :four_of_a_kind, score: 6 }
+    when -> (values) { ([2, 3] & values).size == 2 } then { name: :full_house, score: 5 }
     when -> (values) { values.include?(3) } then { name: :three_of_a_kind, score: 4 }
-    when -> (values) { [1, 2, 2] == values.sort } then { name: :two_pair, score: 5 }
-    when -> (values) { values.include?(2) } then { name: :pair, score: 6 }
+    when -> (values) { [1, 2, 2] == values.sort } then { name: :two_pair, score: 3 }
+    when -> (values) { values.include?(2) } then { name: :pair, score: 2 }
     else
-        { name: :high_card, score: 7 }
+        { name: :high_card, score: 1 }
     end
 end
 
@@ -24,4 +24,4 @@ hands.sort_by! do |hand|
         hand[:hand].each_char.map { |char| CARD_RANK.index(char) }
     ]
 end
-p -> { hands.reverse.map.with_index { |hand, index| hand[:bid] * (index + 1) }.sum }.call
+p -> { hands.map.with_index { |hand, index| hand[:bid] * (index + 1) }.sum }.call
